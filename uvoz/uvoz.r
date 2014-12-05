60e7775d101d0791cb0162b18b0f1873a5fae674
Jakov Kavčič <jakov.kavcic@hotmail.com>
2014-12-05 21:41:01 +0100

    Uvoz
    Ustvaril sem program za uvoz podatkov in razdelil tabelo na dve, eno za
    rezultate v procentih in eno za realne rezultate.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,41 @@
-# 2. faza: Uvoz podatkov
-
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
+uvoziCounty <- function() {
+  return(read.csv("podatki/CY/COUNTY.csv", sep = ",",dec = ".", as.is = TRUE,
+                      header = TRUE,
+                      na.strings = ".",
                       row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
                       fileEncoding = "Windows-1250"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+# Zapisemo podatke v razpredelnico county.
+cat("Zapisem celotne podatke o volitvah\n")
+county <- uvoziCounty()
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+# Manjkajoce podatke sem zamenjal z 0.
+county$NOTA <- as.numeric(county$NOTA)
+county[is.na(county)] <- 0
+
+#Zdruzil sem lokalne kandidate v eden stolpec OTHER.
+county <- data.frame(county[,1:9],OTHER=apply(county[,9:23],1,sum))
+
+
+
+
+# Razdelim rezultate v procentih in odanih glasovih.
+procenti <- data.frame(county[1],county[2],PBUSH=county$PBUSH,PGORE=county$PGORE,PNADER=county$PNADER,POTHER=county$POTHER)
+
+odaniGlasovi <- data.frame(county[1],county[2],county[,7:10])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
